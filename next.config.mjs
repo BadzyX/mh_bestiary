df278ccee0101d33faacb0b57745104255bd062a
Benjamin Peuple <benjamin@bemaos.fr>
2024-06-29 18:20:19 +0200

    update(config): adding react strict mode & declare ENV

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        API_BASE_URL: process.env.API_BASE_URL,
+        API_URL: process.env.API_URL
+    }
+};
 
 export default nextConfig;
